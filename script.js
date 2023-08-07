a97957b8b14399aa9db65599bae10df23c55f71c
Nico17b <blondeau.n@gmail.com>
2023-08-07 22:19:19 +0200

    new changes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.onload = function () {
   const image = document.getElementById('bouncing-image');
   const fileInput = document.getElementById('logo-upload');
+  const themeSwitcher = document.getElementById('theme-switcher');
+  let isDarkTheme = false; // Set the default theme to false (default theme)
   let x = 0;
   let y = 0;
   let xSpeed = 5;
@@ -9,15 +11,48 @@ window.onload = function () {
   const screenHeight = window.innerHeight;
 
   fileInput.addEventListener('change', handleLogoUpload);
+  themeSwitcher.addEventListener('click', toggleTheme);
 
   function handleLogoUpload(event) {
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = function () {
-        image.src = reader.result;
+        const img = new Image();
+        img.src = reader.result;
+
+        img.onload = function () {
+          if (img.width > screenWidth || img.height > screenHeight) {
+            // Resize the image proportionally to fit within the screen
+            const ratio = Math.min(screenWidth / img.width, screenHeight / img.height);
+            image.width = img.width * ratio;
+            image.height = img.height * ratio;
+          } else {
+            image.width = img.width;
+            image.height = img.height;
+          }
+
+          // Reset x position to center
+          x = screenWidth / 2 - image.width / 2;
+          // Reset y position to center
+          y = screenHeight / 2 - image.height / 2;
+
+          // Set the resized image as the source for the bouncing image
+          image.src = img.src;
+        };
       };
       reader.readAsDataURL(file);
+    }
+  }
+
+  function toggleTheme() {
+    isDarkTheme = !isDarkTheme;
+    if (isDarkTheme) {
+      document.body.style.backgroundColor = '#222'; // Dark theme background color
+      themeSwitcher.innerText = 'Default Theme';
+    } else {
+      document.body.style.backgroundColor = '#fff'; // Default theme background color
+      themeSwitcher.innerText = 'Dark Theme';
     }
   }
 
